3743a586f6b9e8a8288a4925ee486286b11bb71e
Tamir_Hazut <tamirhazut1994@gmail.com>
2020-12-30 20:25:45 +0200

    Institutes and Degrees dropdowns added

--- app/src/main/java/com/example/scholarme/Form_Fragment.java
+++ app/src/main/java/com/example/scholarme/Form_Fragment.java
@@ -27,6 +27,10 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -37,7 +41,8 @@ public class Form_Fragment extends Fragment {
 
     private TextView form1_fragment_title;
     private TextView form1_fragment_current_year;
-    private AutoCompleteTextView form1_fragment_DDM;
+    private AutoCompleteTextView form1_fragment_DDM_institute;
+    private AutoCompleteTextView form1_fragment_DDM_degree;
     private RadioGroup form1_fragment_year_radio;
     private DatePicker form1_fragment_graduation_cal;
     private RadioGroup form1_fragment_gander_radio;
@@ -64,7 +69,8 @@ public class Form_Fragment extends Fragment {
 
     private void findviews(View view) {
         this.form1_fragment_title=view.findViewById(R.id.form1_fragment_title);
-        this.form1_fragment_DDM=view.findViewById(R.id.form1_fragment_DDM);
+        this.form1_fragment_DDM_institute=view.findViewById(R.id.form1_fragment_DDM_institute);
+        this.form1_fragment_DDM_degree=view.findViewById(R.id.form1_fragment_DDM_degree);
         this.form1_fragment_graduation_cal=(DatePicker)view.findViewById(R.id.form1_fragment_graduation_cal);
         this.form1_fragment_year_radio=view.findViewById(R.id.form1_fragment_year_radio);
         this.form1_fragment_gander_radio=(RadioGroup)view.findViewById(R.id.form1_fragment_gander_radio);
@@ -75,32 +81,20 @@ public class Form_Fragment extends Fragment {
 
 
     private void initviews(View view) {
-        /*
-        List<Long> numberList = new ArrayList<>();
-        for (int i = 1; i <= 4; i++) {
-            numberList.add(new Long(i));
-        }
-        this.form1_fragment_DDM.setAdapter(
-                new ArrayAdapter<Long>(getActivity().getApplicationContext(), R.layout.dropdown_menu_list_item, numberList));
-        this.form1_fragment_DDM.addTextChangedListener(new TextWatcher() {
+        this.form1_fragment_DDM_institute.setAdapter(new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.dropdown_menu_list_item, readTextFile(R.raw.institutes)));
+        this.form1_fragment_DDM_institute.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                try {
-//                    currentGuest.setNumberOfGuests(Long.parseLong(s.toString()));
-//                } catch (NumberFormatException ex) {
-//                    currentGuest.setNumberOfGuests(null);
-//                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("DDM", "Institute: " + parent.getItemAtPosition(position).toString());
             }
         });
-        */
+        this.form1_fragment_DDM_degree.setAdapter(new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.dropdown_menu_list_item, readTextFile(R.raw.degrees)));
+        this.form1_fragment_DDM_degree.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("DDM", "Degree: " + parent.getItemAtPosition(position).toString());
+            }
+        });
 
         //Gander Buttons action
         form1_fragment_continue_button.setOnClickListener(new View.OnClickListener() {
@@ -118,5 +112,25 @@ public class Form_Fragment extends Fragment {
                 Log.d("pttt", "onClick="+currentYearButton.getText());
             }
         });
+    }
+
+    public List<String> readTextFile(int file) {
+        List<String> data = new ArrayList<>();
+        String string = "";
+        InputStream is = this.getResources().openRawResource(file);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        try {
+            while (true) {
+                try {
+                    if ((string = reader.readLine()) == null) break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (!string.isEmpty()) { data.add(string.replace('\n', '\0')); }
+            }
+            is.close();
+        } finally {
+            return data;
+        }
     }
 }
