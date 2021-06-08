77bf02d60eb63c3df65101ffa9f2b40e6bd4023f
MiriK313 <85124420+MiriK313@users.noreply.github.com>
2021-06-08 10:13:20 +0300

    Update Publish_Scholarship_Fragment.java

--- app/src/main/java/com/example/scholarme/Publish_Scholarship_Fragment.java
+++ app/src/main/java/com/example/scholarme/Publish_Scholarship_Fragment.java
@@ -9,12 +9,14 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 
+    /**
+     * Not Finished
+     */
 public class Publish_Scholarship_Fragment extends Fragment_Base {
 
     private EditText scholarship;
     private EditText description;
     private EditText date;
-
     private EditText c;
 
     private Call_Back_Publish_Scholarship call_back_publish_scholarship;
@@ -33,61 +35,15 @@ public class Publish_Scholarship_Fragment extends Fragment_Base {
         View view= inflater.inflate(R.layout.publish_scholar,container,false);
         findviews(view);
         initviews(view);
-        //Glide.with(this).load("https://image.freepik.com/free-vector/abstract-blue-background-science-technology-graphic-design_44392-180.jpg").into(this.form1_fragment_background_IMG);
         return view;
     }
 
     private void findviews(View view) {
-//        this.scholarship=view.findViewById(R.id.scholarship);
-//        this.name=view.findViewById(R.id.name);
-//        this.year=view.findViewById(R.id.year);
-//        this.description=view.findViewById(R.id.description);
-//        this.date=view.findViewById(R.id.date);
-//        this.email=view.findViewById(R.id.email);
-//        //this.form1_fragment_continue_button=(Button) view.findViewById(R.id.form1_fragment_continue_button);
+
     }
 
 
     private void initviews(View view) {
-//        this.form1_fragment_DDM_institute.setAdapter(new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.dropdown_menu_list_item, readTextFile(R.raw.institutes)));
-//        this.form1_fragment_DDM_institute.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Log.d("DDM", "Institute: " + parent.getItemAtPosition(position).toString());
-//            }
-//        });
-//        this.form1_fragment_DDM_degree.setAdapter(new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.dropdown_menu_list_item, readTextFile(R.raw.degrees)));
-//        this.form1_fragment_DDM_degree.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Log.d("DDM", "Degree: " + parent.getItemAtPosition(position).toString());
-//            }
-//        });
-
-        //Gander Buttons action
-//        form1_fragment_continue_button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                int selectedGender=form1_fragment_gander_radio.getCheckedRadioButtonId();
-//                radioSexButton=(RadioButton) view.findViewById(selectedGender);
-//                Toast.makeText(view.getContext(),radioSexButton.getText(),Toast.LENGTH_SHORT).show();
-//                //
-//                Log.d("pttt", "onClick="+radioSexButton.getText());
-//                Log.d("pttt","Selected Date: "+ form1_fragment_graduation_cal.getDayOfMonth()+"/"
-//                        + (form1_fragment_graduation_cal.getMonth() + 1)+"/"+form1_fragment_graduation_cal.getYear());
-//                Log.d("pttt", "onClick="+form1_fragment_study_year.getText());
-//            }
-//        });
-
-//        this.form1_fragment_continue_button.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                next_form_fragment=new Next_Form_Fragment();
-//                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-//                transaction.replace(R.id.main_layout, next_form_fragment ); // give your fragment container id in first parameter
-//                transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
-//                transaction.commit();
-//            }
-//        });
 
 
     }
