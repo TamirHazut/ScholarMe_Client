88f85684ab7b4ff41d51775764116b6255a0b08e
MiriK313 <85124420+MiriK313@users.noreply.github.com>
2021-06-08 10:23:36 +0300

    Update Results_Fragment.java

--- app/src/main/java/com/example/scholarme/Results_Fragment.java
+++ app/src/main/java/com/example/scholarme/Results_Fragment.java
@@ -43,8 +43,6 @@ public class Results_Fragment extends Fragment_Base {
         scholarships = gson.fromJson(getArguments().getString(Constants.SCHOLARSHIPS), listType);
         findviews(view);
         initImageBitmaps();
-        //initviews(view);
-        //Glide.with(this).load("https://image.freepik.com/free-vector/abstract-blue-background-science-technology-graphic-design_44392-180.jpg").into(this.form1_fragment_background_IMG);
         return view;
     }
 
@@ -53,6 +51,10 @@ public class Results_Fragment extends Fragment_Base {
         this.recyclerView = view.findViewById(R.id.recyclerView);
     }
 
+     /**
+     * Bitmap matching percents to fit image percents
+     */
+    
     private void initImageBitmaps(){
         List<ScholarshipAdapterItem> items = new ArrayList<>();
         for (Scholarship scholarship: scholarships) {
@@ -60,73 +62,15 @@ public class Results_Fragment extends Fragment_Base {
             int image_url_id = this.getActivity().getApplicationContext().getResources().getIdentifier(image_url, "drawable", this.getActivity().getApplicationContext().getPackageName());
             items.add(new ScholarshipAdapterItem(image_url_id, scholarship.getName(), scholarship.getDescription(), scholarship.getMatchingPercentage()));
         }
-        /*Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
 
-        mImageUrls.add(R.drawable.p10);
-        mNames.add("Havasu Falls");
-
-        mImageUrls.add(R.drawable.p100);
-        mNames.add("Trondheim");
-
-        mImageUrls.add(R.drawable.p20);
-        mNames.add("Portugal");
-
-        mImageUrls.add(R.drawable.p60);
-        mNames.add("Rocky Mountain National Park");
-
-        mImageUrls.add(R.drawable.p90);
-        mNames.add("Rocky Mountain National Park");
-
-
-        mImageUrls.add(R.drawable.p50);
-        mNames.add("Portugal");
-
-        mImageUrls.add(R.drawable.p30);
-        mNames.add("Rocky Mountain National Park");
-        mImageUrls.add(R.drawable.p40);
-        mNames.add("Trondheim");
-
-        mImageUrls.add(R.drawable.p80);
-        mNames.add("Portugal");
-
-        mImageUrls.add(R.drawable.p70);
-        mNames.add("Rocky Mountain National Park");
-
-//        mImageUrls.add(R.drawable.med);
-//        mNames.add("Havasu Falls");
-//
-//        mImageUrls.add(R.drawable.huge);
-//        mNames.add("Trondheim");
-//
-//        mImageUrls.add(R.drawable.small);
-//        mNames.add("Portugal");
-//
-//        mImageUrls.add(R.drawable.med);
-//        mNames.add("Rocky Mountain National Park");
-//
-//        mImageUrls.add(R.drawable.med);
-//        mNames.add("Havasu Falls");
-//
-//        mImageUrls.add(R.drawable.small);
-//        mNames.add("Trondheim");
-//
-//        mImageUrls.add(R.drawable.huge);
-//        mNames.add("Havasu Falls");
-//
-//        mImageUrls.add(R.drawable.huge);
-//        mNames.add("Trondheim");
-//
-//        mImageUrls.add(R.drawable.big);
-//        mNames.add("Havasu Falls");
-//
-//        mImageUrls.add(R.drawable.big);
-//        mNames.add("Trondheim");
-
-*/
         initRecyclerView(items);
 
     }
-
+      /**
+     * Gets list of results 
+     *@retun RecyclerView whitch displays the most fit scholarship in more pixels of the screen
+     */
+    
     private void initRecyclerView(List<ScholarshipAdapterItem> items){
         Log.d(TAG, "initRecyclerView: initializing staggered recyclerview.");
 
